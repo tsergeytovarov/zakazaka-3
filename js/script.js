c1725301b5c981f378fae9a8d52173f568663d36
Сергей <tovarov.piter@gmail.com>
2015-02-11 14:59:25 +0400

    Нормализация кода

--- js/script.js
+++ js/script.js
@@ -1,11 +1,18 @@
 $(function(){
 
-  $(".js-toggle-tooltip").on("click", function(){
-    $(this).parents(".tooltip").toggleClass("tooltip--open");
-  })
+  /* Обработка тултипов */
 
-  $(".js-close-tooltip").on("click", function(){
-    $(this).parents(".tooltip").removeClass("tooltip--open");
-  })
+  // откртие и закрытие по ссылке
+    $(".js-toggle-tooltip").on("click", function(){
+      $(".tooltip").removeClass("tooltip--open");
+      $(this).parents(".tooltip").toggleClass("tooltip--open");
+      return false;
+    })
+
+  //  закрытие по крестику
+    $(".js-close-tooltip").on("click", function(){
+      $(this).parents(".tooltip").removeClass("tooltip--open");
+      return false;
+    })
 
 })
